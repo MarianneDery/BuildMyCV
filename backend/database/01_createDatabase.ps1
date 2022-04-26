30fe74612a1617214cdd6a96c36c5f099737aa48
mdery022 <mdery022@uottawa.ca>
2022-04-26 14:50:37 -0400

    Finished the initial database structure - Added constraints to the schema - 01_createDatabase.ps1 populates the database after creating it
    Jira [BMCV-1]

--- backend/database/01_createDatabase.ps1
+++ backend/database/01_createDatabase.ps1
@@ -5,5 +5,40 @@ Get-Content config.txt | Where-Object {$_.length -gt 0} | Where-Object {!$_.Star
 
 $dburl="postgresql://$($User):$($Password)@$($Server):$($Port)/$($Database)"
 
+# SCHEMA
+
 psql -f schema.sql $dburl
-psql -f seed.sql $dburl
+
+# SEED
+
+psql -c "INSERT INTO Users (username, password) VALUES ('example', 'password');" $dburl
+
+$data = "SELECT userID FROM Users WHERE username = 'example'" | psql --csv $dburl | ConvertFrom-Csv
+$userID = $data.userID
+
+psql -c "INSERT INTO About (name, title, about, userID) VALUES ('John Doe', 'Job Title', NULL, '$($userID)');" $dburl
+
+psql -c "INSERT INTO Projects (title, type, description, userID) VALUES ('Project Example', 'Example', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam egestas nunc ligula, a rutrum orci vulputate pharetra. Donec sit amet convallis eros. Cras consectetur et tortor eu ullamcorper. Praesent a dapibus odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec quis nisl ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum orci ut leo finibus placerat.
+Maecenas vitae est eu est fermentum malesuada id vitae magna. Quisque pellentesque ullamcorper feugiat. Nullam in ultricies risus. Integer venenatis ante non condimentum hendrerit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi id dui ac magna feugiat vulputate a sed justo. Vestibulum posuere velit est, at volutpat orci cursus ut.
+Vivamus fringilla nibh et sagittis sagittis. Duis eget mauris at nibh rutrum fringilla feugiat vel augue. Pellentesque finibus, diam et pulvinar tempus, lorem ipsum sodales sapien, eget dictum neque leo eu enim. Sed luctus dui massa, at tempus dolor mollis et. Etiam non euismod mi, non venenatis urna. Aliquam erat volutpat. Fusce faucibus lacus a eros ornare aliquet. Duis in elit vitae dui viverra consectetur. Cras vel tortor a lectus sollicitudin tempor vel vel nisi. Ut ipsum ex, iaculis sed.', '$($userID)');" $dburl
+
+$data = "SELECT projectID FROM Projects" | psql --csv $dburl | ConvertFrom-Csv
+$projectID = $data.projectID
+
+psql -c "INSERT INTO ProjectLinks (text, link, projectID) VALUES ('Link', 'https://www.lipsum.com/', '$($projectID)');
+    INSERT INTO Education (diploma, school, startDate, endDate, userID) VALUES ('High School Degree', 'High School X', '2016-08-01', '2021-05-31', '$($userID)');
+    INSERT INTO Awards (name, date, description, userID) VALUES ('Award Name', '2018-06-16', 'Award for the best grades.', '$($userID)');" $dburl
+
+$data = "SELECT awardID FROM Awards" | psql --csv $dburl | ConvertFrom-Csv
+$awardID = $data.awardID
+
+psql -c "INSERT INTO AwardLinks (text, link, awardID) VALUES ('Link', 'https://www.google.com/', '$($awardID)');
+    INSERT INTO SkillSections (name, userID) VALUES ('Other', '$($userID)');" $dburl
+
+$data = "SELECT skillSectionID FROM SkillSections" | psql --csv $dburl | ConvertFrom-Csv
+$skillSectionID = $data.skillSectionID
+
+psql -c "INSERT INTO Skills (name, level, skillSectionID, userID) VALUES ('Skill 1', 'Beginner', '$($skillSectionID)', '$($userID)');
+    INSERT INTO Skills (name, level, skillSectionID, userID) VALUES ('Skill 2', 'Skilled', NULL, '$($userID)');
+    INSERT INTO Experience (organization, jobTitle, startDate, endDate, description, userID) VALUES ('Organization Name', 'Job Title', '2022-02-15', NULL, '- Task X \n - Task Y', '$($userID)');
+    INSERT INTO Media (mediaType, text, link, userID) VALUES ('LinkedIn', 'LinkedIn', 'https://ca.linkedin.com/', '$($userID)');" $dburl
--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -16,120 +16,134 @@ DROP TABLE IF EXISTS Users;
 DROP TYPE IF EXISTS Level;
 DROP TYPE IF EXISTS MediaType;
 
+DROP FUNCTION IF EXISTS check_skill_section_user;
+
 CREATE TABLE Users (
     userID uuid DEFAULT uuid_generate_v4 (),
-    username VARCHAR(20),
-    password VARCHAR(256),
-    PRIMARY KEY (userID)
+    username VARCHAR(20) UNIQUE NOT NULL,
+    password VARCHAR(256) NOT NULL,
+    CONSTRAINT pk_Users PRIMARY KEY (userID)
 );
 
 CREATE TABLE About (
     aboutID uuid DEFAULT uuid_generate_v4 (),
-    name VARCHAR(255),
-    title VARCHAR(255),
-    about VARCHAR(255),
-    userID uuid,
-    FOREIGN KEY (userID) REFERENCES Users (userID),
-    PRIMARY KEY (aboutID)
+    name VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    about VARCHAR(5000),
+    userID uuid UNIQUE NOT NULL,
+    CONSTRAINT fk_About_userID FOREIGN KEY (userID) REFERENCES Users (userID) ON DELETE CASCADE,
+    CONSTRAINT pk_About PRIMARY KEY (aboutID)
 );
 
 CREATE TABLE  Projects (
     projectID uuid DEFAULT uuid_generate_v4 (),
-    title VARCHAR(255),
+    title VARCHAR(255) NOT NULL,
     type VARCHAR(255),
-    description VARCHAR(255),
-    userID uuid,
-    FOREIGN KEY (userID) REFERENCES Users (userID),
-    PRIMARY KEY (projectID)
+    description VARCHAR(5000),
+    userID uuid NOT NULL,
+    CONSTRAINT fk_Projects_userID FOREIGN KEY (userID) REFERENCES Users (userID) ON DELETE CASCADE,
+    CONSTRAINT pk_Projects PRIMARY KEY (projectID)
 );
 
 CREATE TABLE ProjectLinks (
     projectLinkID uuid DEFAULT uuid_generate_v4 (),
     text VARCHAR(255),
-    link VARCHAR(2048),
-    projectID uuid,
-    FOREIGN KEY (projectID) REFERENCES Projects (projectID),
-    PRIMARY KEY (projectLinkID)
+    link VARCHAR(2048) NOT NULL,
+    projectID uuid NOT NULL,
+    CONSTRAINT fk_ProjectLinks_projectID FOREIGN KEY (projectID) REFERENCES Projects (projectID) ON DELETE CASCADE,
+    CONSTRAINT pk_ProjectLinks PRIMARY KEY (projectLinkID)
 );
 
 CREATE TABLE Education (
     educationID uuid DEFAULT uuid_generate_v4 (),
-    diploma VARCHAR(255),
-    school VARCHAR(255),
-    startDate DATE,
-    endDate DATE,
-    userID uuid,
-    FOREIGN KEY (userID) REFERENCES Users (userID),
-    PRIMARY KEY (educationID)
+    diploma VARCHAR(255) NOT NULL,
+    school VARCHAR(255) NOT NULL,
+    startDate DATE NOT NULL,
+    endDate DATE NOT NULL,
+    userID uuid NOT NULL,
+    CONSTRAINT fk_Education_userID FOREIGN KEY (userID) REFERENCES Users (userID) ON DELETE CASCADE,
+    CONSTRAINT pk_Education PRIMARY KEY (educationID)
 );
 
 CREATE TABLE Awards (
     awardID uuid DEFAULT uuid_generate_v4 (),
-    name VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
     date DATE,
-    description VARCHAR(255),
-    userID uuid,
-    FOREIGN KEY (userID) REFERENCES Users (userID),
-    PRIMARY KEY (awardID)
+    description VARCHAR(5000),
+    userID uuid NOT NULL,
+    CONSTRAINT fk_Awards_userID FOREIGN KEY (userID) REFERENCES Users (userID) ON DELETE CASCADE,
+    CONSTRAINT pk_Awards PRIMARY KEY (awardID)
 );
 
 CREATE TABLE AwardLinks (
     awardLinkID uuid DEFAULT uuid_generate_v4 (),
     text VARCHAR(255),
-    link VARCHAR(2048),
-    awardID uuid,
-    FOREIGN KEY (awardID) REFERENCES Awards (awardID),
-    PRIMARY KEY (awardLinkID)
+    link VARCHAR(2048) NOT NULL,
+    awardID uuid NOT NULL,
+    CONSTRAINT fk_AwardLinks_awardID FOREIGN KEY (awardID) REFERENCES Awards (awardID) ON DELETE CASCADE,
+    CONSTRAINT pk_AwardLinks PRIMARY KEY (awardLinkID)
 );
 
 CREATE TABLE SkillSections (
     skillSectionID uuid DEFAULT uuid_generate_v4 (),
-    name VARCHAR(255),
-    userID uuid,
-    FOREIGN KEY (userID) REFERENCES Users (userID),
-    PRIMARY KEY (skillSectionID)
+    name VARCHAR(255) NOT NULL,
+    userID uuid NOT NULL,
+    CONSTRAINT fk_SkillSections_userID FOREIGN KEY (userID) REFERENCES Users (userID) ON DELETE CASCADE,
+    CONSTRAINT pk_SkillSections PRIMARY KEY (skillSectionID)
 );
 
 CREATE TYPE LEVEL AS ENUM ('Beginner', 'Average', 'Skilled', 'Expert');
 
 CREATE TABLE Skills (
     skillID uuid DEFAULT uuid_generate_v4 (),
-    name VARCHAR(255),
-    level VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
+    level VARCHAR(255) NOT NULL,
     skillSectionID uuid,
-    userID uuid,
-    FOREIGN KEY (skillSectionID) REFERENCES SkillSections (skillSectionID),
-    FOREIGN KEY (userID) REFERENCES Users (userID),
-    PRIMARY KEY (skillID)
+    userID uuid NOT NULL,
+    CONSTRAINT fk_Skills_skillSectionID FOREIGN KEY (skillSectionID) REFERENCES SkillSections (skillSectionID) ON DELETE SET NULL,
+    CONSTRAINT fk_Skills_userID FOREIGN KEY (userID) REFERENCES Users (userID) ON DELETE CASCADE,
+    CONSTRAINT pk_Skills PRIMARY KEY (skillID)
 );
+
+CREATE FUNCTION check_skill_section_user() RETURNS trigger AS $check_skill_section_user$
+    BEGIN
+        IF ((NEW.skillSectionID IS NOT NULL) AND ((SELECT COUNT(S.skillSectionID) FROM SkillSections S WHERE S.skillSectionID = NEW.skillSectionID AND S.userID = NEW.userID) =  0)) THEN
+            RAISE EXCEPTION 'The skillSectionID has to belong to the user defined by userID.';
+        END IF;
+        RETURN NEW;
+    END;
+$check_skill_section_user$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_skill_section_user BEFORE INSERT OR UPDATE ON Skills
+    FOR EACH ROW EXECUTE PROCEDURE check_skill_section_user();
 
 CREATE TABLE Experience (
     experienceID uuid DEFAULT uuid_generate_v4 (),
-    organization VARCHAR(255),
-    jobTitle VARCHAR(255),
-    startDate DATE,
-    endDate DATE,
-    description VARCHAR(255),
-    userID uuid,
-    FOREIGN KEY (userID) REFERENCES Users (userID),
-    PRIMARY KEY (experienceID)
+    organization VARCHAR(255) NOT NULL,
+    jobTitle VARCHAR(255) NOT NULL,
+    startDate DATE NOT NULL,
+    endDate DATE,                                   /*Note: An endDate of Null means the user is currently employed by the company*/
+    description VARCHAR(5000),
+    userID uuid NOT NULL,
+    CONSTRAINT fk_Experience_userID FOREIGN KEY (userID) REFERENCES Users (userID) ON DELETE CASCADE,
+    CONSTRAINT pk_Experience PRIMARY KEY (experienceID)
 );
 
 CREATE TYPE MediaType AS ENUM ('Twitter', 'LinkedIn', 'GitHub', 'Other');
 
 CREATE TABLE Media (
     mediaID uuid DEFAULT uuid_generate_v4 (),
-    mediaType MediaType,
+    mediaType MediaType NOT NULL,
     text  VARCHAR(255),
-    link  VARCHAR(2048),
-    userID uuid,
-    FOREIGN KEY (userID) REFERENCES Users (userID),
-    PRIMARY KEY (mediaID)
+    link  VARCHAR(2048) NOT NULL,
+    userID uuid NOT NULL,
+    CONSTRAINT fk_Media_userID FOREIGN KEY (userID) REFERENCES Users (userID) ON DELETE CASCADE,
+    CONSTRAINT pk_Media PRIMARY KEY (mediaID)
 );
 
 CREATE TABLE Migrations (
     migrationID uuid DEFAULT uuid_generate_v4 (),
-    migrationName  VARCHAR(255),
+    migrationName  VARCHAR(255) NOT NULL,
     date DATE,
-    PRIMARY KEY (migrationID)
+    CONSTRAINT pk_Migrations PRIMARY KEY (migrationID)
 );
